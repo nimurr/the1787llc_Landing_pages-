89f90542f0301d812aaa98bef9334fc9a90953da
nimurr <nimurnerob404@gmail.com>
2025-12-08 16:19:49 +0600

    `Added react-hot-toast library and Toaster component to App.tsx, updated package-lock.json and package.json accordingly`

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
-
+import toast, { Toaster } from 'react-hot-toast';
 import React, { useState } from 'react';
 import Dashboard from './components/Dashboard';
 import LandingPage from './components/LandingPage';
 
 const App: React.FC = () => {
   const [hasAccess, setHasAccess] = useState(false);
+  const paymentAccess = false;
 
   // Simulates a successful checkout/login
   const handleEnterApp = () => {
+    if (!paymentAccess) {
+      toast.error('Payment First for access all.');
+      return
+    };
     setHasAccess(true);
   };
 
   return (
     <>
+      <Toaster />
       {hasAccess ? (
         <Dashboard />
       ) : (
